56810dbf66b88db9b082e0cee3112e9e65c64cd7
Miklos Megyes <m.miklos05@gmail.com>
2024-08-29 01:33:17 +0200

    shadcn ui

--- src/app/@sidemenu/default.tsx
+++ src/app/@sidemenu/default.tsx
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import Link from 'next/link'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 export async function generateStaticParams() {
   // This function ensures the page is generated at build time
@@ -12,14 +13,16 @@ export default async function Default() {
   const filenames = await fs.promises.readdir(dataDirectory)
 
   return (
-    <ul>
-      {filenames.map((filename) => (
-        <li key={filename}>
-          <Link href={`/${path.basename(filename, path.extname(filename))}`}>
-            {path.basename(filename, path.extname(filename))}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <ScrollArea className="bg-muted border-r border-border p-4 h-screen">
+      <ul>
+        {filenames.map((filename) => (
+          <li key={filename}>
+            <Link href={`/${path.basename(filename, path.extname(filename))}`}>
+              {path.basename(filename, path.extname(filename))}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </ScrollArea>
   )
 }
