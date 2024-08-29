7dd7e6e5dab36418704a0f6f0aafd034c3922ce4
Miklos Megyes <m.miklos05@gmail.com>
2024-08-29 13:03:50 +0200

    wip

--- src/app/[arxiv_id]/page.tsx
+++ src/app/[arxiv_id]/page.tsx
@@ -4,6 +4,7 @@ import path from 'path'
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { ExternalLinkIcon } from 'lucide-react'
+import { notFound } from 'next/navigation'
 
 export async function generateStaticParams() {
   const dataDirectory = path.join(process.cwd(), 'data')
@@ -20,38 +21,47 @@ export default async function Page({
 }: {
   params: { arxiv_id: string }
 }) {
-  const content = await fs.promises.readFile(
-    path.join(process.cwd(), 'data', `${params.arxiv_id}.json`),
-    'utf8',
-  )
-  const data = JSON.parse(content) as {
-    arxivId: string
-    summary: string
-    title: string
-    timestamp: string
-  }
+  const filePath = path.join(process.cwd(), 'data', `${params.arxiv_id}.json`)
 
-  return (
-    <div className="max-w-2xl">
-      <h2 className="text-2xl font-bold">{data.title}</h2>
-      <div className="flex flex-row gap-2">
-        <span className="text-sm text-muted-foreground">
-          {new Date(data.timestamp).toLocaleDateString()}
-        </span>
-      </div>
-      <Button
-        variant="link"
-        className="text-blue-500 hover:text-blue-400 pl-0"
-        asChild
-      >
-        <Link href={`https://arxiv.org/pdf/${params.arxiv_id}`} target="_blank">
-          {`https://arxiv.org/pdf/${params.arxiv_id}`}
-          <ExternalLinkIcon className="w-4 h-4 ml-2" />
-        </Link>
-      </Button>
-      <div className="flex flex-col gap-4">
-        <Markdown>{data.summary}</Markdown>
-      </div>
-    </div>
-  )
+  try {
+    const content = await fs.promises.readFile(filePath, 'utf8')
+    const data = JSON.parse(content) as {
+      arxivId: string
+      summary: string
+      title: string
+      timestamp: string
+    }
+
+    return (
+      <>
+        <h2 className="text-2xl font-bold">{data.title}</h2>
+        <div className="flex flex-row gap-2">
+          <span className="text-sm text-muted-foreground">
+            {new Date(data.timestamp).toLocaleDateString()}
+          </span>
+        </div>
+        <Button
+          variant="link"
+          className="text-blue-500 hover:text-blue-400 pl-0"
+          asChild
+        >
+          <Link
+            href={`https://arxiv.org/pdf/${params.arxiv_id}`}
+            target="_blank"
+          >
+            {`https://arxiv.org/pdf/${params.arxiv_id}`}
+            <ExternalLinkIcon className="w-4 h-4 ml-2" />
+          </Link>
+        </Button>
+        <div className="flex flex-col gap-4">
+          <Markdown>{data.summary}</Markdown>
+        </div>
+      </>
+    )
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code === 'ENOENT') {
+      notFound()
+    }
+    throw error
+  }
 }
